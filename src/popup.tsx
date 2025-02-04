98b417f9545dc3871c50ee5f2e0fa99f2c3c5a45
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-04 18:04:41 +0000

    initial commit

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,13 @@
-import { CountButton } from "~features/count-button"
-
 import "~style.css"
 
 function IndexPopup() {
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+      <a
+        target="_blank"
+        href="chrome-extension://npcfjbmndnhebnfdipfegbhcgmpfonjd/tabs/options.html">
+        Click here to go to options
+      </a>
     </div>
   )
 }
---
+++ src/tabs/options.tsx
@@ -0,0 +1,10 @@
+function OptionsPage() {
+  return (
+    <div>
+      <h1>I am the options page</h1>
+      <h2>Hear me roar</h2>
+    </div>
+  )
+}
+
+export default OptionsPage
