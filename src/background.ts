bb2fd715b6444fa1c271cc84db69335b7f03a95f
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-07 10:01:06 +0000

    icon changes a couple of times

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,8 @@
+import green16 from "url:../assets/greenCircle16x16.png";
+import green32 from "url:../assets/greenCircle32x32.png";
+import red16 from "url:../assets/redCircle16x16.png";
+import red32 from "url:../assets/redCircle32x32.png";
+
 import { Storage } from "@plasmohq/storage";
 
 import { getIsEnabled, setIsEnabled } from "~utils/storageHandler";
@@ -8,5 +13,34 @@ chrome.commands.onCommand.addListener(async (command) => {
   if (command === "toggle-blocker") {
     const currentState = await getIsEnabled();
     await setIsEnabled(!currentState);
+  }
+});
+
+const changeIcon = async () => {
+  const isEnabled = await getIsEnabled();
+  if (isEnabled) {
+    chrome.action.setIcon({
+      path: {
+        16: red16,
+        32: red32
+      }
+    });
+  } else {
+    chrome.action.setIcon({
+      path: {
+        16: green16,
+        32: green32
+      }
+    });
+  }
+};
+
+changeIcon();
+
+storage.watch({
+  isEnabled: async (change) => {
+    if (change.newValue) {
+      await changeIcon();
+    }
   }
 });
