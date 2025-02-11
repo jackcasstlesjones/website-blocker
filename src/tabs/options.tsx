8479862bdaf777abdb5e1f44aef57e51535bd62a
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2025-02-11 16:31:51 +0000

    ui refactor

--- src/tabs/options.tsx
+++ src/tabs/options.tsx
@@ -43,7 +43,7 @@ function OptionsPage() {
     await setIsEnabled(newState);
   };
 
-  const handleWebsiteRemove = async (index) => {
+  const handleWebsiteRemove = async (index: number) => {
     await removeWebsite(index);
     const updatedData = await storageGetter();
     setData(updatedData);
@@ -61,63 +61,67 @@ function OptionsPage() {
   }, []);
 
   return (
-    <div className="plasmo-flex plasmo-flex-col plasmo-justify-center plasmo-items-center plasmo-h-28-2">
-      <button
-        onClick={handleEnabledClick}
-        className={`plasmo-w-64 plasmo-h-40 plasmo-text-5xl plasmo-rounded-md plasmo-mt-10 plasmo-mb-10 focus:plasmo-ring-8 focus:plasmo-ring-blue-400 ${isEnabled ? "plasmo-bg-green-500" : "plasmo-bg-red-500"}`}>
-        {isEnabled ? `Unblock` : `Block`}
-      </button>
-      <h1 className="plasmo-text-5xl plasmo-text-center plasmo-mb-10">
-        Options
-      </h1>
-      <p className="plasmo-text-lg plasmo-mb-2">
-        The websites you are currently blocking are:
-      </p>
-      <div className="plasmo-flex plasmo-flex-col plasmo-gap-2">
-        {data
-          ? data.map((website, index) => {
-              return (
-                <div className="plasmo-flex plasmo-gap-2">
-                  <p className="plasmo-text-lg" key={index}>
-                    {website.url}
-                  </p>
-                  <button
-                    onClick={async () => handleWebsiteRemove(index)}
-                    className="plasmo-text-lg plasmo-bg-red-600 plasmo-px-2   plasmo-text-white">
-                    x
-                  </button>
-                </div>
-              );
-            })
-          : undefined}
-      </div>
-      <form
-        action="submit"
-        onSubmit={handleSubmit}
-        className="plasmo-gap-5 plasmo-justify-center plasmo-items-center plasmo-flex plasmo-p-5 plasmo-flex-col">
-        <label className="plasmo-mt-10 plasmo-text-lg" htmlFor="url">
-          Enter the base URL of the website you want to block
-        </label>
-        <input
-          value={websiteUrl}
-          className="plasmo-border-2 plasmo-border-black plasmo-w-52 plasmo-text-lg"
-          name="url"
-          type="text"
-          onChange={(e) => setWebsiteUrl(e.target.value)}
-        />
+    <div className="plasmo-flex plasmo-justify-between">
+      <div className="plasmo-flex plasmo-flex-col plasmo-justify-center plasmo-items-center plasmo-h-28-2 plasmo-bg-gray-100 plasmo-w-8/12">
+        {/* <button */}
+        {/*   onClick={handleEnabledClick} */}
+        {/*   className={`plasmo-w-64 plasmo-h-40 plasmo-text-5xl plasmo-rounded-md plasmo-mt-10 plasmo-mb-10 focus:plasmo-ring-8 focus:plasmo-ring-blue-400 ${isEnabled ? "plasmo-bg-green-500" : "plasmo-bg-red-500"}`}> */}
+        {/*   {isEnabled ? `Unblock` : `Block`} */}
+        {/* </button> */}
+        <h1 className="plasmo-text-5xl plasmo-text-center plasmo-mb-10">
+          Options
+        </h1>
+        <form
+          action="submit"
+          onSubmit={handleSubmit}
+          className="plasmo-gap-5 plasmo-justify-center plasmo-items-center plasmo-flex plasmo-p-5 plasmo-flex-col">
+          <label className="plasmo-mt-10 plasmo-text-lg" htmlFor="url">
+            Enter the base URL of the website you want to block
+          </label>
+          <input
+            value={websiteUrl}
+            className="plasmo-border-2 plasmo-border-black plasmo-w-52 plasmo-text-lg"
+            name="url"
+            type="text"
+            onChange={(e) => setWebsiteUrl(e.target.value)}
+          />
 
+          <button
+            className="plasmo-bg-green-600 plasmo-text-white plasmo-p-5 plasmo-rounded-md plasmo-text-lg"
+            type="submit">
+            Add website
+          </button>
+        </form>
         <button
-          className="plasmo-bg-green-600 plasmo-text-white plasmo-p-5 plasmo-rounded-md plasmo-text-lg"
-          type="submit">
-          Add website
+          onClick={handleClear}
+          className="plasmo-bg-red-600 plasmo-text-white plasmo-p-5 plasmo-rounded-md plasmo-mt-4 plasmo-mb-10"
+          type="button">
+          Clear All Blocked Websites
         </button>
-      </form>
-      <button
-        onClick={handleClear}
-        className="plasmo-bg-red-600 plasmo-text-white plasmo-p-5 plasmo-rounded-md plasmo-mt-4 plasmo-mb-10"
-        type="button">
-        Clear All Blocked Websites
-      </button>
+      </div>
+      <div className="plasmo-shadow-lg plasmo-w-4/12 plasmo-h-screen">
+        <p className="plasmo-my-10 plasmo-text-center plasmo-text-2xl">
+          Currently blocked
+        </p>
+        <div className="plasmo-flex plasmo-flex-wrap  plasmo-gap-5 plasmo-bg-gray-200 plasmo-shadow-sm plasmo-p-10 plasmo-w-10/12 plasmo-m-auto plasmo-rounded-md">
+          {data
+            ? data.map((website, index) => {
+                return (
+                  <div className="plasmo-flex plasmo-gap-3 plasmo-bg-white plasmo-rounded-full plasmo-p-1">
+                    <p className="plasmo-text-lg plasmo-pl-3" key={index}>
+                      {website.url}
+                    </p>
+                    <button
+                      onClick={async () => handleWebsiteRemove(index)}
+                      className="plasmo-text-lg plasmo-px-2   ">
+                      x
+                    </button>
+                  </div>
+                );
+              })
+            : undefined}
+        </div>
+      </div>
     </div>
   );
 }
